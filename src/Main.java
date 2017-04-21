e4448ee8b2720857be8ffd0b7b36e06497dca0ae
mojotintin <mojotintin.outlook>
2017-04-21 23:09:54 +0800

    add 151th

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        TwoSumII_InputArrayISorted_167 sol = new TwoSumII_InputArrayISorted_167();
-        System.out.println(sol.twoSum_2(new int[]{2, 7, 11, 15}, 13)[0] + " " +
-                sol.twoSum_2(new int[]{2, 7, 11, 15}, 13)[1] );
+        ReverseWordsInAString_151 sol = new ReverseWordsInAString_151();
+        System.out.println(sol.reverseWords("the sky is blue"));
     }
 }
--- src/solution/ReverseWordsInAString_151.java
+++ src/solution/ReverseWordsInAString_151.java
@@ -2,6 +2,18 @@ package solution;
 
 /**
  * Created by Calabash on 2017/4/21.
+ *
  */
 public class ReverseWordsInAString_151 {
+
+    public String reverseWords(String s) {
+        if (s.trim() == "") return "";
+        String[] words = s.trim().split("\\s+");
+        String res = "";
+        for (String word : words) {
+            res = word + " " + res;
+        }
+
+        return res.trim();
+    }
 }
