0db9477804151bb5b7a8f7a706941f0ea587b65b
mojotintin <mojotintin@outlook.com>
2017-04-24 15:33:56 +0800

    83th Accepted

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,5 @@ public class Main {
 
     public static void main(String[] args) {
 
-        TreeNode root = new TreeNode(1);
-        root.left = new TreeNode(2);
-        root.right = new TreeNode(3);
-        
     }
 }
