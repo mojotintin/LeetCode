b765a916be0698203fa78fef713d499a1144bfb4
mojotintin <mojotintin@outlook.com>
2017-04-25 14:44:35 +0800

    add 42th

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int[] height = new int[]{0,1,0,2,1,0,1,3,2,1,2,1};
+        TrappingRainWater_42 sol = new TrappingRainWater_42();
+        System.out.println(sol.trap(height));
     }
 }
