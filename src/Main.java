fe7e1ebddfee90e713d123fcb4614c4654df4dd9
mojotintin <mojotintin@outlook.com>
2017-05-02 16:03:31 +0800

    add 2th

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import entities.ListNode;
 import entities.TreeNode;
 import solution.*;
 
@@ -5,7 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        CountAndSay_38 sol = new CountAndSay_38();
-        System.out.println(sol.countAndSay(6));
+        ListNode l1 = new ListNode(5);
+        ListNode l2 = new ListNode(5);
+        AddTwoNumbers_2 sol = new AddTwoNumbers_2();
+        System.out.println(sol.addTwoNumbers(l1, l2).toString());
     }
 }
---
+++ src/solution/AddTwoNumbers_2.java
@@ -0,0 +1,62 @@
+package solution;
+
+import entities.ListNode;
+
+
+/**
+ * Created by Calabash on 2017/5/2.
+ *
+ */
+public class AddTwoNumbers_2 {
+
+    // too redundant
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) return null;
+        ListNode sumList = new ListNode(0);
+        ListNode reList = sumList;
+        ListNode tailNode = null;
+        int overTen = 0;
+        while (l1 != null || l2 != null) {
+            if (l1 == null) {
+                if (l2.val + overTen >= 10) {
+                    sumList.val = l2.val + overTen - 10;
+                    overTen = 1;
+                } else {
+                    sumList.val = l2.val + overTen;
+                    overTen = 0;
+                }
+                l2 = l2.next;
+            } else if (l2 == null){
+                if (l1.val + overTen >= 10) {
+                    sumList.val = l1.val + overTen - 10;
+                    overTen = 1;
+                } else {
+                    sumList.val = l1.val + overTen;
+                    overTen = 0;
+                }
+                l1 = l1.next;
+            } else {
+                if (l1.val + l2.val + overTen >= 10){
+                    sumList.val = l1.val + l2.val + overTen - 10;
+                    overTen = 1;
+                } else {
+                    sumList.val = l1.val + l2.val + overTen;
+                    overTen = 0;
+                }
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+            sumList.next = new ListNode(0);
+            tailNode = sumList;
+            sumList = sumList.next;
+        }
+
+        if (overTen == 1) {
+            sumList.val = 1;
+        } else {
+            tailNode.next = null;
+        }
+
+        return reList;
+    }
+}
