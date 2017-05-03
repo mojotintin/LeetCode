787e2d0878574df63c9abc1f7f26ff9bb0f007bd
mojotintin <mojotintin@outlook.com>
2017-05-03 12:36:29 +0800

    add 566th

---
+++ src/solution/ReshapeTheMatrix_566.java
@@ -0,0 +1,28 @@
+package solution;
+
+/**
+ * Created by Calabash on 2017/5/3.
+ *
+ */
+public class ReshapeTheMatrix_566 {
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+
+        if (nums.length == 0) return nums;
+        if (nums.length * nums[0].length != r * c) return nums;
+        int m = 0, n = 0;
+        int[][] reshapedNums = new int[r][c];
+        for (int[] num : nums) {
+            for (int j = 0; j < nums[0].length; j++) {
+                reshapedNums[m][n] = num[j];
+                if (n + 1 == c) {
+                    n = 0;
+                    m++;
+                } else {
+                    n++;
+                }
+            }
+        }
+
+        return reshapedNums;
+    }
+}
