7dfa045f07a5221716c57591f7a9e34ca13f22cf
mojotintin <mojotintin@outlook.com>
2017-05-08 15:18:13 +0800

    3th, 49th

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,5 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-        ListNode node = new ListNode(1);
-        node.next = new ListNode(2);
-        node.next.next = new ListNode(3);
-        node.next.next.next = new ListNode(4);
-        node.next.next.next.next = node;
-
-        LinkedListCycle_141 sol = new LinkedListCycle_141();
-        System.out.println(sol.hasCycle(node));
     }
 }
