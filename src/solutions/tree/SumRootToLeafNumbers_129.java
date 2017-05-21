20c304aa859dfe5e07cf89be58c4d1b461de9677
mojotintin <mojotintin@outlook.com>
2017-05-21 15:30:51 +0800

    add 129th

---
+++ src/solutions/tree/SumRootToLeafNumbers_129.java
@@ -0,0 +1,35 @@
+package solutions.tree;
+
+import entities.TreeNode;
+
+/**
+ * Created by Calabash on 2017/5/21.
+ * LeetCode 129th : Sum Root to Leaf Numbers
+ * Description : https://leetcode.com/problems/sum-root-to-leaf-numbers/#/description
+ * Difficulty : Medium
+ * Tags : Tree, Depth-First Search
+ */
+public class SumRootToLeafNumbers_129 {
+
+    /**
+     * Submission : 1
+     * Status : Accepted
+     * Runtime : 1ms
+     */
+    public int sumNumbers(TreeNode root) {
+        return sumOfRootToLeaf(root, 0);
+    }
+
+    public int sumOfRootToLeaf(TreeNode root, int sum) {
+        if(root ==null) return sum;
+    	if (root.left == null && root.right == null) {
+    	    return sum * 10 + root.val;
+    	} else if (root.left == null) {
+    		return sumOfRootToLeaf(root.right, sum * 10 + root.val);
+    	} else if (root.right == null) {
+    		return sumOfRootToLeaf(root.left, sum * 10 + root.val);
+    	} else {
+    		return sumOfRootToLeaf(root.left, sum * 10 + root.val) + sumOfRootToLeaf(root.right, sum * 10 + root.val);
+    	}
+    }
+}
