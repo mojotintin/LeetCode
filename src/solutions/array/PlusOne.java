ec864aa1fae7493f27232f3282007d43e3c30996
mojotintin <mojotintin@outlook.com>
2017-06-07 16:54:01 +0800

    add 66 : Plus One

---
+++ src/solutions/array/PlusOne.java
@@ -0,0 +1,37 @@
+package solutions.array;
+
+/**
+ * Created by Calabash on 2017/6/7.
+ * Problem Number : 66
+ * Problem Name : Plus One
+ * Description : https://leetcode.com/problems/plus-one/#/description
+ * Difficulty : Easy
+ * Tags : Array, Math
+ */
+public class PlusOne {
+  /**
+   * Submission : 1
+   * Status : Accepted
+   * Runtime : 0ms
+   */
+  public int[] plusOne(int[] digits) {
+    int over = 1;
+    for (int i = digits.length - over; i >= 0; i--) {
+      if (digits[i] + over >= 10) {
+        digits[i] = digits[i] + over - 10;
+      } else {
+        digits[i] = digits[i] + over;
+        over = 0;
+        break;
+      }
+    }
+    if (over == 0) {
+      return digits;
+    } else {
+      int[] newDigits = new int[digits.length + 1];
+      newDigits[0] = 1;
+      System.arraycopy(digits, 0, newDigits, 1, digits.length);
+      return newDigits;
+    }
+  }
+}
