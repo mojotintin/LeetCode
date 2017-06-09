3c4880efbcc0c6235a82e2cd97a065aa1dcf6380
mojotintin <mojotintin@outlook.com>
2017-06-09 10:58:34 +0800

    add 117 : Populating Next Right Pointers in Each Node II

---
+++ src/solutions/tree/PopulatingNextRightPointersInEachNodeII.java
@@ -0,0 +1,50 @@
+package solutions.tree;
+
+import entities.TreeLinkNode;
+
+/**
+ * Created by Calabash on 2017/6/9.
+ * Problem Number : 117
+ * Problem Name : Populating Next Right Pointers in Each Node II
+ * Description : https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/#/description
+ * Difficulty : Medium
+ * Tags : Tree, Depth-first Search
+ */
+public class PopulatingNextRightPointersInEachNodeII {
+
+  /**
+   * Submission : 1
+   * Status : Accepted
+   * Runtime : 2ms
+   */
+  public void connect(TreeLinkNode root) {
+    if (root == null) {
+      return;
+    }
+    TreeLinkNode node = root;
+    while (node != null) {
+      TreeLinkNode pre = null;
+      TreeLinkNode start = null;
+      while (node != null) {
+        if (node.left != null) {
+          if (pre != null) {
+            pre.next = node.left;
+          } else {
+            start = node.left;
+          }
+          pre = node.left;
+        }
+        if (node.right != null) {
+          if (pre != null) {
+            pre.next = node.right;
+          } else {
+            start = node.right;
+          }
+          pre = node.right;
+        }
+        node = node.next;
+      }
+      node = start;
+    }
+  }
+}
