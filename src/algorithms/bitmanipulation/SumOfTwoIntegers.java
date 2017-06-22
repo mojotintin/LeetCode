b3f2357726066e56a78189cba71fbb2943b8e860
葫芦娃八娃 <mojotintin@outlook.com>
2017-06-22 15:07:46 +0800

    Add 371 - Sum of Two Integer

---
+++ src/algorithms/bitmanipulation/SumOfTwoIntegers.java
@@ -0,0 +1,30 @@
+package algorithms.bitmanipulation;
+
+/**
+ * Created by calabash on 2017/6/22.
+ * Problem Number : 371
+ * Problem Name : Sum of Two Integers
+ * Description :
+ * Difficulty : Easy
+ * Tags : Bit Manipulation
+ */
+public class SumOfTwoIntegers {
+
+  /**
+   * Submission : 1
+   * Status : Accepted
+   * Runtime : 0ms
+   */
+  public int getSum(int a, int b) {
+    int sum   = a ^ b;
+    int carry = (a & b) << 1;
+    while (carry != 0) {
+      a = sum;
+      b = carry;
+      sum   = a ^ b;
+      carry = (a & b) << 1;
+    }
+    return sum;
+  }
+}
+
