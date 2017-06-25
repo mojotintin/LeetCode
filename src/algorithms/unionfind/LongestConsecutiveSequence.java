1cdb611c5bc92bc61a84adbc575c1a3b2878ad0f
mojotintin <mojotintin@outlook.com>
2017-06-25 13:01:38 +0800

    Add a Solution for 128 - Longest Consecutive Sequence

--- src/algorithms/unionfind/LongestConsecutiveSequence.java
+++ src/algorithms/unionfind/LongestConsecutiveSequence.java
@@ -41,11 +41,13 @@ public class LongestConsecutiveSequence {
     }
     return len;
   }
+
   /**
    * Submission : 2
    * Status : Accepted
    * Runtime : 17ms
    * Time Complexity : O(n)
+   * Note : Hash Map
    */
   public int longestConsecutive_2(int[] nums) {
     Map<Integer, Boolean> map = new HashMap<>();
@@ -64,6 +66,37 @@ public class LongestConsecutiveSequence {
         while (map.containsKey(down)) {
           map.replace(down, false);
           down -= 1;
+        }
+        if (len < up - down - 1) {
+          len = up - down - 1;
+        }
+      }
+    }
+    return len;
+  }
+
+  /**
+   * Submission : 3
+   * Status : Accepted
+   * Runtime : 11ms
+   * Time Complexity : O(n)
+   * Note : Hash Set
+   */
+  public int longestConsecutive_3(int[] nums) {
+    Set<Integer> set = new HashSet<>();
+    for (int num : nums) {
+      set.add(num);
+    }
+    int len = 0;
+    for (int num : nums) {
+      if (set.contains(num)) {
+        int up = num + 1;
+        int down = num - 1;
+        while (set.contains(up)) {
+          set.remove(up++);
+        }
+        while (set.contains(down)) {
+          set.remove(down--);
         }
         if (len < up - down - 1) {
           len = up - down - 1;
