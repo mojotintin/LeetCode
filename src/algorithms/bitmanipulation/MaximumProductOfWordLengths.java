08ad48cff9c5e45692874253ffec9851ae30becb
葫芦娃八娃 <mojotintin@outlook.com>
2017-08-01 20:18:52 +0800

    Add a Solution for Problem 318 - Maximum Product of Word Lengths

--- src/algorithms/bitmanipulation/MaximumProductOfWordLengths.java
+++ src/algorithms/bitmanipulation/MaximumProductOfWordLengths.java
@@ -37,11 +37,34 @@ public class MaximumProductOfWordLengths {
    * Status: Accepted
    * Runtime: 78ms
    */
-  public int maxProduct(String[] words) {
+  public int maxProduct_1(String[] words) {
     int[] tags = new int[words.length];
     for (int i = 0; i < words.length; i++) {
       for (int j = 0; j < words[i].length(); j++) {
         tags[i] =tags[i] | (int)Math.pow(2, words[i].charAt(j) - 'a');
+      }
+    }
+    int max = 0;
+    for (int i = 0; i < words.length - 1; i++) {
+      for (int j = i + 1; j < words.length; j++) {
+        if ((tags[i] & tags[j]) == 0) {
+          max = Math.max(max, words[i].length() * words[j].length());
+        }
+      }
+    }
+    return max;
+  }
+
+  /**
+   * Submission: 2
+   * Status: Accepted
+   * Runtime: 22ms
+   */
+  public int maxProduct_2(String[] words) {
+    int[] tags = new int[words.length];
+    for (int i = 0; i < words.length; i++) {
+      for (int j = 0; j < words[i].length(); j++) {
+        tags[i] = tags[i] | (1 << words[i].charAt(j) - 'a');
       }
     }
     int max = 0;
